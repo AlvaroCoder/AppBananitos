246d56a33275535e5ff102034e3aba914c04cba4
alvaro <alvaro89_piura@hotmail.com>
2023-06-22 09:40:09 -0500

    Comandos registro

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,34 @@
 const routes = require('express').Router();
-const { RegisterModel } = require('../services/Database');
-routes.get("/",async (req,res)=>{
-    const data = await RegisterModel.getRegisters();
-    res.send(data)
+const { RegisterModel, PlantModel } = require('../services/Database');
+routes.get("/dates",async(req,res)=>{
+    const response = await RegisterModel.getDatesRegister();
+    res.send(response);
+});
+routes.get("/",async(req,res)=>{
+    const header = req.headers;
+    const fechaCreacion = header['fechacreacion']
+    const response_registers = await RegisterModel.getRegisterByFecha(fechaCreacion);
+    console.log(fechaCreacion);
+    const registers = response_registers.message
+    let values = registers.map(async(val)=>{
+        const idRegistro = val['idRegistro'];
+        const values = (await PlantModel.getValuesPlantByIdRegistro(idRegistro)).message;
+        let object = {
+            ubicacion : val['ubicacion'],
+            valores_tipo : values
+        }
+        return object
+    });
+    let valores_generales =(await PlantModel.getValuesGeneralPlanta(fechaCreacion)).message
+    Promise.all(values).then((data)=>{
+        res.send({
+            err : false,
+            valores : data,
+            valores_generales
+        })
+    })
+    
+    
 });
 routes.post("/",async(req,res)=>{
     const body = req.body;
@@ -11,7 +37,7 @@ routes.post("/",async(req,res)=>{
     const idRegister = await RegisterModel.createRegister(json_body)
     res.send({
         err : false,
-        message : 2
+        message : idRegister
     })
 });
 module.exports = routes;
--- services/Database.js
+++ services/Database.js
@@ -5,6 +5,10 @@ const GET_REGISTERS = process.env.GET_REGISTERS;
 const GET_LOCATIONID_BY_VALUE = process.env.GET_LOCATIONID_BY_VALUE
 const CREATE_REGISTER = process.env.CREATE_REGISTER
 const CREATE_PLANT = process.env.CREATE_PLANT
+const GET_DATES_REGISTER = "SELECT fechaCreacion FROM registro GROUP BY fechaCreacion ORDER BY fechaCreacion DESC;"
+const GET_REGISTER_BY_FECHA_CREACION = "SELECT idRegistro, ubicacion FROM registro WHERE fechaCreacion = ?"
+const GET_VALUES_PLANT_BY_ID_REGISTER = "SELECT tipo AS Nombre, SUM(valor) AS Total, MIN(valor) AS Minimo, MAX(valor) AS Maximo, AVG(valor) AS Promedio FROM planta WHERE idRegistro = ? GROUP BY tipo;"
+const GET_VALUES_GENERAL_REGISTER = "SELECT  COUNT(*) AS Nro_plantas, SUM(planta.valor) AS Total, AVG(planta.valor) AS promedio FROM planta, registro WHERE registro.idRegistro = planta.idRegistro AND registro.fechaCreacion = ?;"
 const RegisterModel = {
     getRegisters : async ()=>{
         try{
@@ -32,6 +36,39 @@ const RegisterModel = {
             return {
                 err : true,
                 message : err
+            }
+        }
+    },
+    getDatesRegister : async()=>{
+        try{
+            const dates = await pool.query(GET_DATES_REGISTER).then((res)=>res[0])
+            let json_dates = dates.map((val)=>{
+                const d = new Date(val['fechaCreacion'])
+                return d.toDateString()
+            })
+            return {
+                err : false,
+                message : json_dates
+            }
+        }catch(err){
+            console.log(err);
+            return {
+                err:true,
+                message : null
+            }
+        }
+    },
+    getRegisterByFecha : async(fechaCreacion)=>{
+        try{
+            const response = await pool.query(GET_REGISTER_BY_FECHA_CREACION,[fechaCreacion]).then((val)=>val[0]);
+            return {
+                err : false,
+                message : response
+            }
+        }catch(err){
+            return {
+                err : true,
+                message : null
             }
         }
     }
@@ -79,6 +116,35 @@ const PlantModel = {
             return{
                 err : true,
                 message : err
+            }
+        }
+    },
+    getValuesPlantByIdRegistro : async (idRegister)=>{
+        try{
+            const response = await pool.query(GET_VALUES_PLANT_BY_ID_REGISTER,[idRegister]).then((val)=>val[0])
+            return {
+                err : false,
+                message : response
+            }
+
+        }catch(err){
+            return{
+                err : true,
+                message : null
+            }
+        }
+    },
+    getValuesGeneralPlanta : async(fechaCreacion)=>{
+        try{
+            const response = await pool.query(GET_VALUES_GENERAL_REGISTER,[fechaCreacion]).then((val)=>val[0][0]);
+            return{
+                err : false,
+                message : response
+            }
+        }catch(err){
+            return{
+                err:true,
+                message : null
             }
         }
     }
